74317053f8a909e9eb0d9a431afafeaa50e02bf1
Munya-sya <89200417+Munya-sya@users.noreply.github.com>
2022-04-11 00:13:41 +0300

    Update secretary.js

--- src/Components/Pages/secretary.js
+++ src/Components/Pages/secretary.js
@@ -9,7 +9,7 @@ export default class ViewPoll extends Component {
     }
 
         componentDidMount() {
-            axios.get('http://localhost:4000/edit/Secretary')
+            axios.get('https://votingbacken.herokuapp.com/edit/Secretary')
                 .then(res => {
                     this.setState({ usersCollection: res.data,
                     Id: res.data._id  });
